e974451825d07b53533fbb1cc0d28c293ba124f8
liuning0427 <1196488380@qq.com>
2019-05-03 20:56:41 +0800

    app

---
+++ newapp/src/components/Foot.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div>
+            <ul class="footer">
+                <li>消息</li>
+                <li>联系人</li>
+                <li>记事本</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .footer{
+        position: fixed;
+        bottom:0;
+        background: #00a801;
+        height:1rem;
+        width: 100%;
+        display: flex;
+    }
+    .footer li{
+        flex:1;
+        font-size:0.3rem;
+        text-align: center;
+        line-height: 1rem;
+    }
+</style>
---
+++ newapp/src/views/One.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>哈哈</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
